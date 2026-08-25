40fde0b1ac1e5c3ac96cb341b2cec8c5bf88df94
Kexin Liu <kexin.liu@my.utsa.edu>
2026-08-26 02:53:52 +0800

    Finalize defense head exports

--- src/heads/__init__.py
+++ src/heads/__init__.py
@@ -8,15 +8,12 @@ from .source_heads import (
     validate_feature_frame,
 )
 
-__all__ = [
-    "BOOKKEEPING_FEATURES",
-    "NO_BOOKKEEPING_DIM",
-    "NO_BOOKKEEPING_FEATURES",
-    "SourceHeadModels",
-    "feature_matrix",
-    "make_logistic_head",
-    "validate_feature_frame",
-]
+from .human_reference import (
+    HumanReference,
+    HumanReferenceHead,
+    human_oof_scores,
+    matrix_from_df,
+)
 
 from .fake_artifact import (
     FakeArtifactCalibration,
@@ -28,3 +25,27 @@ from .fake_artifact import (
     gesture_metrics,
     is_fake_artifact,
 )
+
+__all__ = [
+    "BOOKKEEPING_FEATURES",
+    "NO_BOOKKEEPING_DIM",
+    "NO_BOOKKEEPING_FEATURES",
+    "SourceHeadModels",
+    "feature_matrix",
+    "make_logistic_head",
+    "validate_feature_frame",
+
+    "HumanReference",
+    "HumanReferenceHead",
+    "human_oof_scores",
+    "matrix_from_df",
+
+    "FakeArtifactCalibration",
+    "artifact_count",
+    "artifact_flags",
+    "calibrate_fake_artifact",
+    "detect_from_artifact_count",
+    "fake_artifact_detect",
+    "gesture_metrics",
+    "is_fake_artifact",
+]
